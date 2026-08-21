104538ffc74f8ec715947dae1341b9c104ec1f16
Maykon Fonseca <mykosistema@gmail.com>
2026-08-20 21:48:59 -0300

    feat: aula01 - implementa classe Passageiro com atributos nome e saldo

---
+++ 130826/src/br/com/fiap/main/SistemaPrincipal.java
@@ -0,0 +1,14 @@
+package br.com.fiap.main;
+
+import br.com.fiap.model.Passageiro;
+
+public class SistemaPrincipal {
+
+    public static void main(String[] args) {
+
+        Passageiro passageiro1 = new Passageiro();
+        passageiro1.nome = "Lorena Lourencini";
+        passageiro1.saldo = 50.0;
+
+    }
+}
---
+++ 130826/src/br/com/fiap/model/Passageiro.java
@@ -0,0 +1,8 @@
+package br.com.fiap.model;
+
+public class Passageiro {
+
+    public String nome;
+    public double saldo;
+
+}
